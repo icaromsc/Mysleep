90347c3fc09516ba0e6eae8b4d385d7fcd0800f0
icarus <icaromscastro@gmail.com>
2016-08-31 11:59:31 -0300

    adicionado metodo para enviar arquivo para servidor

--- app/src/main/java/br/edu/ufcspa/snorlax_angelo/AudioRecorder.java
+++ app/src/main/java/br/edu/ufcspa/snorlax_angelo/AudioRecorder.java
@@ -1,6 +1,7 @@
 package br.edu.ufcspa.snorlax_angelo;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.StatFs;
@@ -9,12 +10,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -48,6 +55,16 @@ public class AudioRecorder extends AppCompatActivity {
     private boolean isRecording = false;
     private boolean isProcessing = false;
     private String fileToprocess = "";
+
+
+    int serverResponseCode = 0;
+    private ProgressDialog dialog = null;
+
+    private String upLoadServerUri = null;
+
+    /**********  File Path *************/
+    final String uploadFilePath = "/mnt/sdcard/";
+    final String uploadFileName = "service_lifecycle.png";
 
     private Chronometer cronometro;
     private Button btn_gravacao;
@@ -83,7 +100,7 @@ public class AudioRecorder extends AppCompatActivity {
             public void onClick(DialogInterface arg0, int arg1) {
 
             }
-        });
+        });                                      //:)
         alerta = builder.create();
 
         txt_cap2 = ((TextView)findViewById(R.id.txt_cap2));
@@ -233,10 +250,12 @@ public class AudioRecorder extends AppCompatActivity {
                 try{
                     inAudioData = new FileInputStream(fileToprocess);
 
-                    while(inAudioData.read(data) != -1){
-                        audioFinal.write(data);
-                    }
 
+
+//                    while(inAudioData.read(data) != -1){
+//                        audioFinal.write(data);
+//                    }
+//
                     inAudioData.close();
                     deleteTempFile(fileToprocess);
 
@@ -476,5 +495,154 @@ public class AudioRecorder extends AppCompatActivity {
         horas = (int)Math.floor((bytesAvailable()/BYTES_HORA));
         return horas;
     }
+
+
+    /*public int uploadFile(String sourceFileUri) {
+
+
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+                    + uploadFilePath + "" + uploadFileName);
+
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    messageText.setText("Source File not exist :"
+                            +uploadFilePath + "" + uploadFileName);
+                }
+            });
+
+            return 0;
+
+        }
+        else
+        {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name="uploaded_file";filename=""
+                                + fileName + """ + lineEnd);
+
+                        dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                    +" http://www.androidexample.com/media/uploads/"
+                                    +uploadFileName;
+
+                            messageText.setText(msg);
+                            Toast.makeText(UploadToServer.this, "File Upload Complete.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("MalformedURLException Exception : check script url.");
+                        Toast.makeText(UploadToServer.this, "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        messageText.setText("Got Exception : see logcat ");
+                        Toast.makeText(UploadToServer.this, "Got Exception : see logcat ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Log.e("Upload file to server Exception", "Exception : "
+                        + e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
+    }
+    */
+
+
+
 
 }
